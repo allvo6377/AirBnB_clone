42f7f7e000e1c9a9c49400971a316652cc15972c
allvo6377 <alvinpilot@gmail.com>
2023-05-10 20:19:33 +0300

    Updated Base model class and linked it to the class FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,26 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
-        """Initialize a new BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize instance attributes"""
+        if kwargs:
+            for key, value in kwargs.item():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
@@ -23,6 +33,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance"""
