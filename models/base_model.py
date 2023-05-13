11b917b9dd4179e5eb479c38bc4770d10de0c831
allvo6377 <alvinpilot@gmail.com>
2023-05-14 02:35:33 +0300

    base_model updated

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,18 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
