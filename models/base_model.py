4f791c4fc3ce58353bd656676594cf296b895453
allvo6377 <alvinpilot@gmail.com>
2023-05-14 04:54:49 +0300

    updated base model -quick fix

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Contains class BaseModel"""
 import uuid
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
@@ -27,6 +28,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance"""
