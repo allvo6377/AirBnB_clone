d858991a0687b6d99f2a231b44b9069b3668ad40
allvo6377 <alvinpilot@gmail.com>
2023-05-14 03:48:21 +0300

    init file updated

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance"""
 
-        from models import storage
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -21,7 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance"""
@@ -29,9 +28,8 @@ class BaseModel:
 
     def save(self):
         """Update the updated_at attribute with the current datetime"""
-        from models import storage
         self.updated_at = datetime.now()
-        self.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance"""
