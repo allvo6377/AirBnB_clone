005978e8aadc8f14817f1222517b15973bf7cdb5
allvo6377 <alvinpilot@gmail.com>
2023-05-14 08:26:30 +0300

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Unit tests for the FileStorage class."""
 import unittest
-import os.path
+import os
+import json
 
-import models
-from models import base_model
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -15,59 +14,47 @@ from models.review import Review
 from models.engine.file_storage import FileStorage
 
 
-
 class TestFileStorage(unittest.TestCase):
-
-    """Unit tests for the FileStorage class."""
-
     def setUp(self):
-        """Sets up the test environment."""
-        self.file_storage = FileStorage()
+        # Create an instance of FileStorage and add some objects to it
+        self.storage = FileStorage()
+        self.user = User()
+        self.state = State()
+        self.storage.new(self.user)
+        self.storage.new(self.state)
+
+    def tearDown(self):
+        # Delete the JSON file created by the tests
+        try:
+            os.remove("file.json")
+        except:
+            pass
 
     def test_all(self):
-        """Tests that the all() method returns an empty dictionary."""
-        self.assertEqual(self.file_storage.all(), {})
+        # Test that the 'all' method returns the correct dictionary of objects
+        objs = self.storage.all()
+        self.assertIn("User." + self.user.id, objs)
+        self.assertIn("State." + self.state.id, objs)
 
     def test_new(self):
-        """Tests that the new() method adds a new object to the dictionary of objects."""
-        user = User("johndoe", "johndoe@example.com", "password")
-        self.file_storage.new(user)
-        self.assertEqual(self.file_storage.all()["User.1"], user)
+        # Test that the 'new' method adds an object to the dictionary of objects
+        user2 = User()
+        self.storage.new(user2)
+        objs = self.storage.all()
+        self.assertIn("User." + user2.id, objs)
 
-    def test_save(self):
-        """Tests that the save() method serializes the object to a JSON file."""
-        user = User("johndoe", "johndoe@example.com", "password")
-        self.file_storage.new(user)
-        self.file_storage.save()
+    def test_save_reload(self):
+        # Test that the 'save' method serializes the objects to the JSON file,
+        # and that the 'reload' method deserializes the objects back into memory
+        self.storage.save()
         with open("file.json", "r") as f:
-            json_dict = json.load(f)
-            self.assertEqual(json_dict["User.1"], user.to_dict())
-
-    def test_reload(self):
-        """Tests that the reload() method deserializes the JSON file to objects."""
-        user = User("johndoe", "johndoe@example.com", "password")
-        self.file_storage.new(user)
-        self.file_storage.save()
-        self.file_storage.reload()
-        self.assertEqual(self.file_storage.all()["User.1"], user)
-
-    def test_add_existing_object(self):
-        """Tests that the new() method raises an exception if you try to add an object that already exists in the database."""
-        user = User("johndoe", "johndoe@example.com", "password")
-        self.file_storage.new(user)
-        with self.assertRaises(Exception):
-            self.file_storage.new(user)
-
-    def test_save_invalid_object(self):
-        """Tests that the save() method raises an exception if you try to save an object that is not a valid model."""
-        with self.assertRaises(Exception):
-            self.file_storage.save({})
-
-    def test_reload_missing_file(self):
-        """Tests that the reload() method raises an exception if the JSON file is missing."""
-        with self.assertRaises(FileNotFoundError):
-            self.file_storage.reload()
-
-
+            data = json.load(f)
+        self.assertIn("User." + self.user.id, data)
+        self.assertIn("State." + self.state.id, data)
+        self.storage = FileStorage()
+        self.storage.reload()
+        objs = self.storage.all()
+        self.assertIn("User." + self.user.id, objs)
+        self.assertIn("State." + self.state.id, objs)
 if __name__ == "__main__":
     unittest.main()
