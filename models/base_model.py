86ab2d4c194867e629bfa60f7698b3e888bc7cb9
allvo6377 <alvinpilot@gmail.com>
2023-05-14 09:59:26 +0300

    updated base-model

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,9 @@ class BaseModel:
         from models import storage
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
