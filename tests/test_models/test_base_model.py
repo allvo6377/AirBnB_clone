8bf6b8f352d12f17c82b788c84f940fa935965bb
allvo6377 <alvinpilot@gmail.com>
2023-05-14 08:47:30 +0300

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,106 +1,54 @@
 #!/usr/bin/python3
 """A module containing unit tests for the base_model class"""
 import unittest
-from datetime import datetime
+from datetime import datetime,timedelta
 from models import storage
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        with self.assertRaises(AttributeError):
+            print(self.bm1.does_not_exist)
 
-    def test_init_with_kwargs(self):
-        """Test initializing a BaseModel with kwargs"""
-        kwargs = {"id": "test_id", "created_at": "2022-01-01T00:00:00.000000", "updated_at": "2022-01-01T00:00:00.000000", "test_attr": "test_value"}
-        bm = BaseModel(**kwargs)
-        self.assertEqual(bm.id, "test_id")
-        self.assertEqual(bm.created_at, datetime(2022, 1, 1))
-        self.assertEqual(bm.updated_at, datetime(2022, 1, 1))
-        self.assertFalse(hasattr(bm, "test_attr"))
-        self.assertIsNotNone(storage.all().get(bm.__class__.__name__ + "." + bm.id))
-
-    def test_init_without_kwargs(self):
-        """Test initializing a BaseModel without kwargs"""
+    def test_save(self):
+        # Test calling the save method on a BaseModel instance
         bm = BaseModel()
-        self.assertIsNotNone(bm.id)
-        self.assertIsNotNone(bm.created_at)
-        self.assertIsNotNone(bm.updated_at)
-        self.assertIsNotNone(storage.all().get(bm.__class__.__name__ + "." + bm.id))
+        original_updated_at = bm.updated_at
+        bm.save()
+        self.assertNotEqual(original_updated_at, bm.updated_at)
 
-    def test_init_with_invalid_kwargs(self):
-        """Test initializing a BaseModel with invalid kwargs"""
-        kwargs = {"invalid_key": "invalid_value"}
-        bm = BaseModel(**kwargs)
-        self.assertIsNotNone(bm.id)
-        self.assertIsNotNone(bm.created_at)
-        self.assertIsNotNone(bm.updated_at)
-        self.assertIsNotNone(storage.all().get(bm.__class__.__name__ + "." + bm.id))
-
-    def test_init_with_short_id(self):
-        """Test initializing a BaseModel with a short id string"""
-        bm = BaseModel(id="1234")
-        self.assertEqual(len(bm.id), 36)
-
-    def test_init_with_long_id(self):
-        """Test initializing a BaseModel with a long id string"""
-        bm = BaseModel(id="123456789012345678901234567890123456")
-        self.assertEqual(len(bm.id), 36)
-
-    def test_init_with_invalid_timestamp(self):
-        """Test initializing a BaseModel with an invalid timestamp string"""
-        kwargs = {"created_at": "2022-01-01", "updated_at": "2022-01-01"}
-        bm = BaseModel(**kwargs)
-        self.assertIsNotNone(bm.id)
-        self.assertIsNotNone(bm.created_at)
-        self.assertIsNotNone(bm.updated_at)
-        self.assertIsNotNone(storage.all().get(bm.__class__.__name__ + "." + bm.id))
-
-    def test_create_many_instances(self):
-        """Test creating many BaseModel instances"""
-        for i in range(10000):
-            bm = BaseModel()
-            self.assertIsNotNone(storage.all().get(bm.__class__.__name__ + "." + bm.id))
-
-    def test_to_dict_with_all_attributes(self):
-        """Test to_dict method with all BaseModel attributes"""
+    def test_to_dict(self):
+        # Test calling the to_dict method on a BaseModel instance
         bm = BaseModel()
         bm_dict = bm.to_dict()
-        self.assertEqual(bm_dict["id"], bm.id)
-        self.assertEqual(bm_dict["created_at"], bm.created_at.isoformat())
-        self.assertEqual(bm_dict["updated_at"], bm.updated_at.isoformat())
-        self.assertEqual(bm_dict["__class__"], bm.__class__.__name__)
-    
-    def test_to_dict_with_extra_attributes(self):
-        """Test to_dict method with extra attributes"""
-        bm = BaseModel()
-        bm.test_attr = "test_value"
-        bm_dict = bm.to_dict()
-        self.assertEqual(bm_dict["id"], bm.id)
-        self.assertEqual(bm_dict["created_at"], bm.created_at.isoformat())
-        self.assertEqual(bm_dict["updated_at"], bm.updated_at.isoformat())
-        self.assertEqual(bm_dict["__class__"], bm.__class__.__name__)
-        self.assertEqual(bm_dict["test_attr"], "test_value")
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
+        self.assertIsInstance(bm_dict["created_at"], str)
+        self.assertIsInstance(bm_dict["updated_at"], str)
+        self.assertEqual(bm_dict["created_at"], str(bm.created_at.isoformat()))
+        self.assertEqual(bm_dict["updated_at"], str(bm.updated_at.isoformat()))
+
+        # Test calling the to_dict method with an argument
+        with self.assertRaises(TypeError):
+            bm.to_dict("invalid_arg")
 
 
-    def test_save_updates_timestamp(self):
-        """Test that save method updates updated_at attribute"""
-        bm = BaseModel()
-        old_updated_at = bm.updated_at
-        bm.save()
-        self.assertNotEqual(old_updated_at, bm.updated_at)
+    def test_equality(self):
+        # Test equality and inequality of BaseModel instances
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+        self.assertEqual(bm1, bm1)
+        self.assertNotEqual(bm1, bm2)
 
-    def test_save_saves_to_storage(self):
-        """Test that save method saves instance to storage"""
-        bm = BaseModel()
-        bm.save()
-        key = bm.__class__.__name__ + "." + bm.id
-        self.assertIsNotNone(storage.all().get(key))
+    def test_created_at(self):
+        # Test that the created_at attribute is set to the current datetime
+        bm1 = BaseModel()
+        now = datetime.now()
+        self.assertLessEqual(bm1.created_at, now)
+        self.assertGreaterEqual(bm1.created_at, now - timedelta(seconds=1))
 
-    def test_str_representation(self):
-        """Test the string representation of BaseModel"""
-        bm = BaseModel()
-        expected = "[BaseModel] ({}) {}".format(bm.id, bm.__dict__)
-        self.assertEqual(str(bm), expected)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_updated_at(self):
+        # Test that the updated_at attribute is set to the current datetime
+        bm1 = BaseModel()
+        now = datetime.now
